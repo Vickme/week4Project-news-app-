82c6bdf2a935cf6415f2baa72328e40ee8ba22a3
victor <victor.njutha@student.moringaschool.com>
2023-10-24 22:30:47 -0700

    creating buttons

---
+++ index.js
@@ -0,0 +1,7 @@
+//variables 
+const genralBtn = document.getElementById("genral");
+const businessBtn = document.getElementById("Business");
+const sportsBtn = document.getElementById("Sports");
+const entertainmentBtn = document.getElementById("Entertainment");
+const technologyBtn = document.getElementById("Technology");
+const searchBtn = document.getElementById("search");
