bf9ce92891ac3868b9da55c954753c177c22b0be
victor <victor.njutha@student.moringaschool.com>
2023-10-25 22:46:52 -0700

    added db.json

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const fetchNews=async (page)=>{
 
     console.log("fetching news...");
-    var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2023-10-25&' +
-          'pageSize=20&' +
-          'page=' +page+
-          'sortBy=popularity&' +
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
           'apiKey=3f123e3f023043158b150d7dec4ec715';
-
 var req = new Request(url);
-
 let a= await fetch(req)
-   let response = await a.json()
-   console.log(response)
+    let response = await a.json()
+        console.log(JSON.stringify(response))
+    
+
+
+   let str =  ` <div class="card my-4 mx-2" style="width: 18rem;">
+   <img src="..." class="card-img-top" alt="...">
+   <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">
+           card's content.</p>
+       <a href="#" class="btn btn-primary">Go somewhere</a>
+   </div>
+</div>`
+
           
 }
 
+fetchNews(1)
