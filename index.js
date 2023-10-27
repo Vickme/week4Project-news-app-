65fa1e2185f1c8ada282f05b901c66edc9b50be9
victor <victor.njutha@student.moringaschool.com>
2023-10-26 19:54:49 -0700

    making button

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+let currentQuery= "Technology"
+let currentPage= 1;
 const fetchNews = async (page, q) => {
+
     var url = 'https://newsapi.org/v2/everything?' +
         'q=' +q+
         '&pageSize=20&' +
@@ -11,17 +14,17 @@ const fetchNews = async (page, q) => {
 
    let a = await fetch(req)
    let response= await a.json()
-   console.log(JSON.stringify(response))
+   
    
     let str = ""
     resultCount.innerHTML = response.totalResults
-    for (let item of response.articles) {
+    for (let item of response.articles){
         str = str + ` <div class="card my-4 mx-2" style="width: 18rem;">
    <img src="${item.urlToImage}" class="card-img-top" alt="...">
    <div class="card-body">
        <h5 class="card-title">${item.title}</h5>
        <p class="card-text">${item.description}</p>
-       <a href="${item.url}" target="_blank" class="btn btn-primary">Go somewhere</a>
+       <a href="${item.url}" target="_blank" class="btn btn-primary">Read more</a>
    </div>
 </div>`
 
@@ -31,7 +34,7 @@ const fetchNews = async (page, q) => {
 
 }
 
-fetchNews()
+fetchNews(1, currentQuery)
 search.addEventListener("click", (e) => {
     e.preventDefault()
     let query = searchInput.value;
@@ -39,3 +42,19 @@ search.addEventListener("click", (e) => {
     fetchNews(1, query)
 })
 
+prev.addEventListener("click", (e) => {
+    e.preventDefault()
+    if ( currentPage> 1){
+        currentPage=  currentPage - 1
+        fetchNews( currentPage , currentQuery)
+    }
+})
+
+next.addEventListener("click", (e) => {
+    e.preventDefault()
+    if ( currentPage > 1){
+        currentPage=  currentPage + 1
+        fetchNews( currentPage , currentQuery)
+    }
+    
+})
